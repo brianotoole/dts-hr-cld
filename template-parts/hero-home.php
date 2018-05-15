c5db4cc2dd06a92aeb7b1d536aff8b41990c6e0f
Brian O'Toole <brian@sparxoo.com>
2018-05-15 12:31:49 -0400

    comment out acf for now

--- template-parts/hero-home.php
+++ template-parts/hero-home.php
@@ -1,8 +1,8 @@
 <?php
 // module 1: HERO
-$hero_heading = get_field('home_hero_heading');
-$hero_subheading = get_field('home_hero_subheading');
-$hero_button = get_field('home_hero_button');
+//$hero_heading = get_field('home_hero_heading');
+//$hero_subheading = get_field('home_hero_subheading');
+//$hero_button = get_field('home_hero_button');
 ?>
 <div class="hero hero--home">
   <div class="hero__inner">
