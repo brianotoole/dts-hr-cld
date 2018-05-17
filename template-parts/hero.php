f83d0bc3e0fc597849e5aaec417f1cf6b6c7c8df
Brian O'Toole <brian@sparxoo.com>
2018-05-17 12:07:43 -0400

    work on hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -15,8 +15,23 @@ $hero_button = get_field('hero_button');
 ?>
 
 <div class="hero">
-  <div class="hero__bg" style="background-image: url(<?php echo $hero_background; ?>);"></div>
-  <div class="hero__inner">
-   hi
-  </div><!--/.hero__inner-->
+  <div class="hero__bg hero__bg--is-full-height" style="background-image: url(<?php echo $hero_background; ?>);">
+    <div class="hero__inner">
+      <div class="container">
+        <?php
+          if ( have_posts() ) :
+            while ( have_posts() ) :
+              the_post();
+          ?>
+            <div class="hero__text">
+              <h1 class="hero__heading h2 u-text-bold u-text-upper"><?php the_field('hero_heading'); ?></h1>
+              <p class="hero__subheading"><?php the_field('hero_subheading'); ?></p>
+            </div><!--/.hero__text-->
+          <?php
+            endwhile;
+          endif;
+          ?>
+        </div><!--/.container-->
+    </div><!--/.hero__inner-->
+  </div><!--/.hero__bg-->
 </div><!--/.hero-->
