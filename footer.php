e45ae8c6031a2b29a83e4078a3fb230679d3d36d
Brian O'Toole <brian@sparxoo.com>
2018-05-17 12:46:33 -0400

    setup footer + footer__bottom

--- footer.php
+++ footer.php
@@ -1,12 +1,25 @@
-<section class="footer">
+<section class="footer footer--secondary">
  <div class="container">
-  <div class="footer__logo">
-    <img src="<?php echo get_template_directory_uri() . '/assets/img/solana-logo-color.png'; ?>" alt="">
-   </div>
-  </div>
+   <div class="footer__top">
+     <div class="footer__top-menu">
+
+     </div>
+   </div><!--/.footer-top-->
+   <div class="footer__bottom">
+     <div class="footer__bottom-copy">&copy; <?php echo bloginfo('name'); ?> <?php echo the_date('Y'); ?></div>
+     <div class="footer__bottom-menu">
+       <ul>
+         <li><a>Sitemap</a></li>
+         <li><a>Terms of Use</a></li>
+         <li><a>Privacy Policy</a></li>
+       </ul>
+     </div><!--/.footer-bottom-menu-->
+   </div><!--/.footer-bottom-->
+
+ </div><!--/.container-->
 </section>
 
- <?php wp_footer(); ?>
-  </div>
+  <?php wp_footer(); ?>
+  </div><!--/#page-->
 </body>
 </html>
