c8ecbb5122865f299baacdc300e0978dd80af2c8
Brian O'Toole <brian@sparxoo.com>
2018-05-22 16:00:26 -0400

    add ‘solution’ post-type

--- functions/add-custom-post-types.php
+++ functions/add-custom-post-types.php
@@ -60,4 +60,65 @@ add_action( 'init', 'create_testimonial_cpt', 0 );
 
 
 
+// Register Custom Post Type Solution
+// Post Type Key: solution
+function create_solution_cpt() {
+
+	$labels = array(
+		'name' => __( 'Solutions', 'Post Type General Name', 'spx' ),
+		'singular_name' => __( 'Solution', 'Post Type Singular Name', 'spx' ),
+		'menu_name' => __( 'Solutions', 'spx' ),
+		'name_admin_bar' => __( 'Solution', 'spx' ),
+		'archives' => __( 'Solution Archives', 'spx' ),
+		'attributes' => __( 'Solution Attributes', 'spx' ),
+		'parent_item_colon' => __( 'Parent Solution:', 'spx' ),
+		'all_items' => __( 'All Solutions', 'spx' ),
+		'add_new_item' => __( 'Add New Solution', 'spx' ),
+		'add_new' => __( 'Add New', 'spx' ),
+		'new_item' => __( 'New Solution', 'spx' ),
+		'edit_item' => __( 'Edit Solution', 'spx' ),
+		'update_item' => __( 'Update Solution', 'spx' ),
+		'view_item' => __( 'View Solution', 'spx' ),
+		'view_items' => __( 'View Solutions', 'spx' ),
+		'search_items' => __( 'Search Solution', 'spx' ),
+		'not_found' => __( 'Not found', 'spx' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'spx' ),
+		'featured_image' => __( 'Featured Image', 'spx' ),
+		'set_featured_image' => __( 'Set featured image', 'spx' ),
+		'remove_featured_image' => __( 'Remove featured image', 'spx' ),
+		'use_featured_image' => __( 'Use as featured image', 'spx' ),
+		'insert_into_item' => __( 'Insert into Solution', 'spx' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Solution', 'spx' ),
+		'items_list' => __( 'Solutions list', 'spx' ),
+		'items_list_navigation' => __( 'Solutions list navigation', 'spx' ),
+		'filter_items_list' => __( 'Filter Solutions list', 'spx' ),
+	);
+	$args = array(
+		'label' => __( 'Solution', 'spx' ),
+		'description' => __( 'Solutions', 'spx' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-list-view',
+		'supports' => array('title', 'thumbnail', ),
+		'taxonomies' => array(),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => true,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'solution', $args );
+
+}
+add_action( 'init', 'create_solution_cpt', 1 );
+
+
+
 ?>
