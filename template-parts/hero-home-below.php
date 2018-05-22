ac0279da988551919bd410c537ff57638d421015
Brian O'Toole <brian@sparxoo.com>
2018-05-22 15:18:37 -0400

    cleanup hero-home-below

--- template-parts/hero-home-below.php
+++ template-parts/hero-home-below.php
@@ -9,13 +9,13 @@
     <div class="row hero-below">
       <div class="col-sm-6 col-xs-12">
         <div class="hero-below__image">
-          [image]
+          <img src="<?php echo get_template_directory_uri() . '/dist/img/home-macbook.png'; ?>">
         </div>
       </div>
       <div class="col-sm-6 col-xs-12">
         <h6 class="hero-below__title u-text-primary">Unify Your Workspace</h6>
         <p class="hero-below__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut animi velit aliquid suscipit at ad dignissimos praesentium! Alias quidem quaerat, voluptatem, ea amet cupiditate quisquam vero officiis commodi temporibus necessitatibus?</p>
-      </div>
-    </div>
-  </div>
+      </div><!--/.col-->
+    </div><!--/.row-->
+  </div><!--/.container-->
 </section>
