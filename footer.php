74a696add86f46b9dec9db78f3ba1067131a2415
Brian O'Toole <brian@sparxoo.com>
2018-05-23 18:30:13 -0400

    footer updates (static for now)

--- footer.php
+++ footer.php
@@ -2,36 +2,40 @@
  <div class="container">
    <div class="footer__top">
      <div class="row start-sm center-xs">
-       <div class="col-sm-3 col-xs-12">
+       <div class="col-sm-3 col-xs-6">
          <ul class="footer__top-menu">
            <div class="footer__top-title">Company</div>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
+           <li><a>Company</a></li>
+           <li><a>About</a></li>
+           <li><a>Blog</a></li>
+           <li><a>Careers</a></li>
          </ul>
        </div>
-       <div class="col-sm-3 col-xs-12">
+       <div class="col-sm-3 col-xs-6">
          <ul class="footer__top-menu">
            <div class="footer__top-title">Solutions</div>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
+           <li><a>Company</a></li>
+           <li><a>About</a></li>
+           <li><a>Blog</a></li>
+           <li><a>Careers</a></li>
          </ul>
        </div>
-       <div class="col-sm-3 col-xs-12">
+       <div class="col-sm-3 col-xs-6">
          <ul class="footer__top-menu">
            <div class="footer__top-title">Resources</div>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
+           <li><a>Company</a></li>
+           <li><a>About</a></li>
+           <li><a>Blog</a></li>
+           <li><a>Careers</a></li>
          </ul>
        </div>
-       <div class="col-sm-3 col-xs-12">
+       <div class="col-sm-3 col-xs-6">
          <ul class="footer__top-menu">
            <div class="footer__top-title">Contact</div>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
-           <li><a>link</a></li>
+           <li><a>Company</a></li>
+           <li><a>About</a></li>
+           <li><a>Blog</a></li>
+           <li><a>Careers</a></li>
          </ul>
        </div>
      </div>
