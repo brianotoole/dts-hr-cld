bc3e7f31d30b4de58c797f6423c9bbe19a912ab0
Brian O'Toole <brian@sparxoo.com>
2018-05-30 10:19:24 -0400

    add gform

--- template-parts/section-cta-home.php
+++ template-parts/section-cta-home.php
@@ -7,11 +7,16 @@ $cta_button_text = get_field('cta_button_text');
 $cta_button_link = get_field('cta_button_link');
 ?>
 
-<section class="section section--secondary">
+<section class="section section--cta-home section--secondary">
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        See Our Solution In Action [ form ]
+    <div class="row cta">
+      <div class="col-sm-4 col-xs-12">
+        <h6 class="cta__title">See Our Solution In Action</h6>
+      </div>
+      <div class="col-sm-8 col-xs-12">
+        <div class="cta__form">
+          <?php echo do_shortcode('[gravityform id=1 title=false description=false]'); ?>
+        </div>
       </div>
     </div>
   </div>
