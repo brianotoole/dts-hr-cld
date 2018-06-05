095d37834b2da0105c78d70ad8cd705342f2d099
Brian O'Toole <brian@sparxoo.com>
2018-06-05 16:53:17 -0400

    add ‘resource’ post type + tax

--- functions/add-custom-post-types.php
+++ functions/add-custom-post-types.php
@@ -121,4 +121,137 @@ add_action( 'init', 'create_solution_cpt', 1 );
 
 
 
+// Register Custom Post Type resource
+// Post Type Key: resource
+function create_resource_cpt() {
+
+	$labels = array(
+		'name' => __( 'Resources', 'Post Type General Name', 'spx' ),
+		'singular_name' => __( 'resource', 'Post Type Singular Name', 'spx' ),
+		'menu_name' => __( 'Resources', 'spx' ),
+		'name_admin_bar' => __( 'resource', 'spx' ),
+		'archives' => __( 'resource Archives', 'spx' ),
+		'attributes' => __( 'resource Attributes', 'spx' ),
+		'parent_item_colon' => __( 'Parent resource:', 'spx' ),
+		'all_items' => __( 'All resources', 'spx' ),
+		'add_new_item' => __( 'Add New resource', 'spx' ),
+		'add_new' => __( 'Add New', 'spx' ),
+		'new_item' => __( 'New resource', 'spx' ),
+		'edit_item' => __( 'Edit resource', 'spx' ),
+		'update_item' => __( 'Update resource', 'spx' ),
+		'view_item' => __( 'View resource', 'spx' ),
+		'view_items' => __( 'View resources', 'spx' ),
+		'search_items' => __( 'Search resource', 'spx' ),
+		'not_found' => __( 'Not found', 'spx' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'spx' ),
+		'featured_image' => __( 'Featured Image', 'spx' ),
+		'set_featured_image' => __( 'Set featured image', 'spx' ),
+		'remove_featured_image' => __( 'Remove featured image', 'spx' ),
+		'use_featured_image' => __( 'Use as featured image', 'spx' ),
+		'insert_into_item' => __( 'Insert into resource', 'spx' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this resource', 'spx' ),
+		'items_list' => __( 'resources list', 'spx' ),
+		'items_list_navigation' => __( 'resources list navigation', 'spx' ),
+		'filter_items_list' => __( 'Filter resources list', 'spx' ),
+	);
+	$args = array(
+		'label' => __( 'resource', 'spx' ),
+		'description' => __( 'Resources for the site', 'spx' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-book',
+		'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author','category' ),
+		'taxonomies' => array('topic', 'type' ),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => true,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'resource', $args );
+
+}
+add_action( 'init', 'create_resource_cpt', 2 );
+
+// Register Taxonomy Topic
+// Taxonomy Key: topic
+function create_topic_tax() {
+
+	$labels = array(
+		'name'              => _x( 'Topics', 'taxonomy general name', 'spx' ),
+		'singular_name'     => _x( 'Topic', 'taxonomy singular name', 'spx' ),
+		'search_items'      => __( 'Search Topics', 'spx' ),
+		'all_items'         => __( 'All Topics', 'spx' ),
+		'parent_item'       => __( 'Parent Topic', 'spx' ),
+		'parent_item_colon' => __( 'Parent Topic:', 'spx' ),
+		'edit_item'         => __( 'Edit Topic', 'spx' ),
+		'update_item'       => __( 'Update Topic', 'spx' ),
+		'add_new_item'      => __( 'Add New Topic', 'spx' ),
+		'new_item_name'     => __( 'New Topic Name', 'spx' ),
+		'menu_name'         => __( 'Topic', 'spx' ),
+	);
+	$args = array(
+		'labels' => $labels,
+		'description' => __( 'Topic taxonomy for "Resource" Post Type', 'spx' ),
+		'hierarchical' => true,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_nav_menus' => true,
+		'show_in_rest' => false,
+		'show_tagcloud' => true,
+		'show_in_quick_edit' => true,
+		'show_admin_column' => false,
+	);
+	register_taxonomy( 'topic', array('resource', ), $args );
+
+}
+add_action( 'init', 'create_topic_tax' );
+
+// Register Taxonomy Type
+// Taxonomy Key: type
+function create_type_tax() {
+
+	$labels = array(
+		'name'              => _x( 'Types', 'taxonomy general name', 'spx' ),
+		'singular_name'     => _x( 'Type', 'taxonomy singular name', 'spx' ),
+		'search_items'      => __( 'Search Types', 'spx' ),
+		'all_items'         => __( 'All Types', 'spx' ),
+		'parent_item'       => __( 'Parent Type', 'spx' ),
+		'parent_item_colon' => __( 'Parent Type:', 'spx' ),
+		'edit_item'         => __( 'Edit Type', 'spx' ),
+		'update_item'       => __( 'Update Type', 'spx' ),
+		'add_new_item'      => __( 'Add New Type', 'spx' ),
+		'new_item_name'     => __( 'New Type Name', 'spx' ),
+		'menu_name'         => __( 'Type', 'spx' ),
+	);
+	$args = array(
+		'labels' => $labels,
+		'description' => __( 'Type taxonomy for "Resource" Post Type', 'spx' ),
+		'hierarchical' => true,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_nav_menus' => true,
+		'show_in_rest' => false,
+		'show_tagcloud' => true,
+		'show_in_quick_edit' => true,
+		'show_admin_column' => false,
+	);
+	register_taxonomy( 'type', array('resource', ), $args );
+
+}
+add_action( 'init', 'create_type_tax' );
+
+
+
 ?>
