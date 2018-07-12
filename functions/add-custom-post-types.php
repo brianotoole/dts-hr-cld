ad364cae278da9b94ced2a85e16527833293ae3e
Brian O'Toole <botoole518@gmail.com>
2018-07-12 19:14:13 -0400

    add ‘team’ post type

--- functions/add-custom-post-types.php
+++ functions/add-custom-post-types.php
@@ -315,4 +315,65 @@ add_action( 'init', 'create_service_cpt', 0 );
 
 
 
+// Register Custom Post Type Team
+// Post Type Key: team
+function create_team_cpt() {
+
+	$labels = array(
+		'name' => __( 'Team', 'Post Type General Name', 'spx' ),
+		'singular_name' => __( 'Team', 'Post Type Singular Name', 'spx' ),
+		'menu_name' => __( 'Team', 'spx' ),
+		'name_admin_bar' => __( 'Team', 'spx' ),
+		'archives' => __( 'Team Archives', 'spx' ),
+		'attributes' => __( 'Team Attributes', 'spx' ),
+		'parent_item_colon' => __( 'Parent Team:', 'spx' ),
+		'all_items' => __( 'All Team', 'spx' ),
+		'add_new_item' => __( 'Add New Team', 'spx' ),
+		'add_new' => __( 'Add New', 'spx' ),
+		'new_item' => __( 'New Team', 'spx' ),
+		'edit_item' => __( 'Edit Team', 'spx' ),
+		'update_item' => __( 'Update Team', 'spx' ),
+		'view_item' => __( 'View Team', 'spx' ),
+		'view_items' => __( 'View Team', 'spx' ),
+		'search_items' => __( 'Search Team', 'spx' ),
+		'not_found' => __( 'Not found', 'spx' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'spx' ),
+		'featured_image' => __( 'Featured Image', 'spx' ),
+		'set_featured_image' => __( 'Set featured image', 'spx' ),
+		'remove_featured_image' => __( 'Remove featured image', 'spx' ),
+		'use_featured_image' => __( 'Use as featured image', 'spx' ),
+		'insert_into_item' => __( 'Insert into Team', 'spx' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Team', 'spx' ),
+		'items_list' => __( 'Team list', 'spx' ),
+		'items_list_navigation' => __( 'Team list navigation', 'spx' ),
+		'filter_items_list' => __( 'Filter Team list', 'spx' ),
+	);
+	$args = array(
+		'label' => __( 'Team', 'spx' ),
+		'description' => __( '', 'spx' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-admin-users',
+		'supports' => array('title', 'editor', 'revisions', ),
+		'taxonomies' => array(),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'team', $args );
+
+}
+add_action( 'init', 'create_team_cpt', 0 );
+
+
+
 ?>
