837057d78563059f76c3a1bfdac83d276d4a41ed
Brian O'Toole <botoole518@gmail.com>
2018-07-18 21:43:06 -0400

    add single service

---
+++ single-service.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<?php
+// HERO
+get_template_part('template-parts/hero', ''); ?>
+
+<section class="section section--default">
+<div class="container">
+  <div class="row">
+    <div clas="col-xs-12">
+      <?php
+      if ( have_posts() ) :
+        while ( have_posts() ) :
+          the_post();
+          the_content();
+      ?>
+      <?php endwhile; else : ?>
+
+      <?php endif; ?>
+    </div><!--/.col-->
+  </div><!--/.row-->   
+</div><!--/.container-->
+</section>
+
+<?php
+// CTA (if yes)
+if (get_field('has_cta') == 'true') :
+  get_template_part('template-parts/section', 'cta');
+endif;
+
+?>
+
+<?php get_footer(); ?>
