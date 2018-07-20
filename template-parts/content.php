7dc8f409a3cae7b1b007b142707a8e8ac81f6067
Brian O'Toole <botoole518@gmail.com>
2018-07-19 20:19:51 -0400

    add section to content default

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" class="section section--entry-content">
 <div class="entry-content">
 	<?php the_content(); ?>
 </div>
