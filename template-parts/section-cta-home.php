e5181970ed2d6f014ec9d2df901b4432d60efbc8
Brian O'Toole <botoole518@gmail.com>
2018-07-23 15:43:01 -0400

    fix home cta responsive

--- template-parts/section-cta-home.php
+++ template-parts/section-cta-home.php
@@ -10,10 +10,10 @@ $cta_button_link = get_field('cta_button_link');
 <section class="section section--cta-home section--secondary">
   <div class="container">
     <div class="row cta">
-      <div class="col-sm-4 col-xs-12">
+      <div class="col-md-4 col-sm-12 col-xs-12">
         <h6 class="cta__title">See Our Solution In Action</h6>
       </div>
-      <div class="col-sm-8 col-xs-12">
+      <div class="col-md-8 col-sm-12 col-xs-12">
         <div class="cta__form">
           <?php echo do_shortcode('[gravityform id=1 title=false description=false ajax=true]'); ?>
         </div>
