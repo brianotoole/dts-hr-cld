9cba78bbe240898a4592ce2aea84da5062887996
Brian O'Toole <botoole518@gmail.com>
2018-07-31 19:25:13 -0400

    rm gform hook to fix post data to pardot issue

--- functions/add-gform-overrides.php
+++ functions/add-gform-overrides.php
@@ -1,25 +1,6 @@
 <?php
 
 /**
- * Move Gravity Forms scripts to footer and
- * delay until jQuery is loaded
- */
-
-// Move the scripts to the footer
-add_filter('gform_init_scripts_footer', '__return_true');
-// Prevent scripts from firing before Google's CDN copy of jQuery is downloaded
-add_filter('gform_cdata_open', 'vtl_wrap_gform_cdata_open');
-function vtl_wrap_gform_cdata_open($content = '') {
-    $content = 'document.addEventListener("DOMContentLoaded", function() { ';
-    return $content;
-}
-add_filter('gform_cdata_close', 'vtl_wrap_gform_cdata_close');
-function vtl_wrap_gform_cdata_close($content = '') {
-    $content = ' }, false );';
-    return $content;
-}
-
-/**
 * Change Grvity Form spinner image (what shows on submit)
 */
 add_filter( 'gform_ajax_spinner_url', 'spinner_url', 10, 2 );
@@ -32,7 +13,7 @@ function spinner_url( $image_src, $form ) {
  * Post Gravity Form data to Pardot
  */
 function gform_post_to_pardot($entry, $form) {
-    $post_url = 'http://go.datis.com/l/106012/2018-05-31/3mg413'; // Form handler endpoint URL
+    $post_url = 'https://go.datis.com/l/106012/2018-05-31/3mg413'; // Form handler endpoint URL
     $body = array(
         'firstname' => rgar($entry, '1'), //'firstname' == name in pardot; '1' == id of form field IN GF admin
         'email' => rgar($entry, '2'),
