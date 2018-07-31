9472082e25f8a4ac0a7efe40529e95526f268dca
Brian O'Toole <botoole518@gmail.com>
2018-07-31 17:50:17 -0400

    add various marketing trackers

--- footer.php
+++ footer.php
@@ -58,5 +58,82 @@
 
   <?php wp_footer(); ?>
   </div><!--/#page-->
+
+<script>
+// Capterra
+var capterra_vkey = 'cc880d56a1ff17c5129509568115807e',
+capterra_vid = '2094770',
+capterra_prefix = (('https:' == document.location.protocol) ? 'https://ct.capterra.com' : 'http://ct.capterra.com');
+(function() {
+
+   var ct = document.createElement('script'); ct.type = 'text/javascript'; ct.async = true;
+
+   ct.src = capterra_prefix + '/capterra_tracker.js?vid=' + capterra_vid + '&vkey=' + capterra_vkey;
+
+   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ct, s);
+
+})();
+
+
+// Pardot
+piAId = '107012';
+
+piCId = '1293';
+
+ 
+
+(function() {
+
+                function async_load(){
+
+                                var s = document.createElement('script'); s.type = 'text/javascript';
+
+                                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+
+                                var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+
+                }
+
+                if(window.attachEvent) { window.attachEvent('onload', async_load); }
+
+                else { window.addEventListener('load', async_load, false); }
+
+})();
+
+
+
+// Addroll
+adroll_adv_id = "2SFBVMTLMVA4HP6VVDZD53";
+
+adroll_pix_id = "PNEJS4ZHYZFVNESMYQLMGX";
+
+(function () {
+
+var oldonload = window.onload;
+
+window.onload = function(){
+
+   __adroll_loaded=true;
+
+   var scr = document.createElement("script");
+
+   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+
+   scr.setAttribute('async', 'true');
+
+   scr.type = "text/javascript";
+
+   scr.src = host + "/j/roundtrip.js";
+
+   ((document.getElementsByTagName('head') || [null])[0] ||
+
+    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+
+   if(oldonload){oldonload()}};
+
+}());
+
+</script>
+<script type="text/javascript"> _linkedin_data_partner_id = "57492"; </script><script type="text/javascript"> (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=57492&fmt=gif" /> </noscript>
 </body>
 </html>
--- header.php
+++ header.php
@@ -12,6 +12,71 @@
 //Custom Field Group == Site Options
 $site_logo = get_field('site_logo', 'option');
 ?>
+<!-- Hotjar Tracking Code for https://www.datis.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:957984,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-37058681-1', 'auto');
+ga('set', '&uid', '9');
+ga('set', 'forceSSL', true);
+ga('send', 'pageview');
+
+	jQuery(document).ready(function(e) {
+    jQuery('a').click(function(e) {
+		var $this = jQuery(this);
+      	var href = $this.prop('href').split('?')[0];
+		var ext = href.split('.').pop();
+		if ('xls,xlsx,doc,docx,ppt,pot,pptx,pdf,pub,txt,zip,rar,tar,7z,gz,exe,wma,mov,avi,wmv,wav,mp3,midi,csv,tsv,jar,psd,pdn,ai,pez,wwf,torrent,cbr'.split(',').indexOf(ext) !== -1) {		
+        ga('send', 'event', 'Download', ext, href);
+      }
+	  if (href.toLowerCase().indexOf('mailto:') === 0) {
+        ga('send', 'event', 'Mailto', href.substr(7));
+      }
+	  if (href.toLowerCase().indexOf('tel:') === 0) {
+        ga('send', 'event', 'Phone number', href.substr(4));
+      }
+      if ((this.protocol === 'http:' || this.protocol === 'https:') && this.hostname.indexOf(document.location.hostname) === -1) {
+        ga('send', 'event', 'Outbound', this.hostname, this.pathname);
+      }
+	});
+});
+
+
+  var _gaq = _gaq || [];
+
+  _gaq.push(['_setAccount', '']);
+
+  _gaq.push(['_trackPageview']);
+
+
+
+  (function() {
+
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+  })();
+
+</script>
+
 
 
 <body <?php body_class(); ?>>
